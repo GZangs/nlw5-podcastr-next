3869abf09e64517013850d3ae8afe10fdb9fc748
Gabriel Zanghelini <gabriel.zanghelini@mutual.club>
2022-02-24 14:52:05 -0300

    main layout setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import { Header } from "../components/Header";
+
+const Home: NextPage = () => {
+  return (
+    <div>Home</div>
+  );
+};
+
+export default Home;
