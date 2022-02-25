26931f7612017e8c653c9f41a2ed881237728a28
Gabriel Zanghelini <zanghelinigab@gmail.com>
2022-02-25 14:51:32 -0300

    listing episodes on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,120 @@
-import type { NextPage } from "next";
-import { Header } from "../components/Header";
+import type { NextPage, GetStaticProps } from "next";
+import Image from "next/image";
+import { format, parseISO } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
-const Home: NextPage = () => {
+import playGreenPic from "../../public/play-green.svg";
+
+import { api } from "../services/api";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+
+import styles from "./home.module.scss";
+
+type IApiEpisode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  published_at: string;
+  description: string;
+  file: {
+    url: string;
+    duration: string;
+  };
+};
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  thumbnail: string;
+  publishedAt: string;
+  description: string;
+  url: string;
+  duration: number;
+  durationAsString: string;
+};
+
+type HomeProps = {
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
+};
+
+const Home: NextPage<HomeProps> = ({ latestEpisodes, allEpisodes }) => {
   return (
-    <div>Home</div>
+    <div className={styles.homePage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+
+        <ul>
+          {latestEpisodes.map((episode) => {
+            return (
+              <li key={episode.id}>
+                <Image
+                  src={episode.thumbnail}
+                  alt={episode.title}
+                  width={192}
+                  height={192}
+                  objectFit="cover"
+                />
+
+                <div className={styles.episodeDetails}>
+                  <a href="">{episode.title}</a>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+
+                <button type="button">
+                  <Image src={playGreenPic} alt="Tocar episódio" />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+      <section className={styles.allEpisodes}></section>
+    </div>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get<IApiEpisode[]>("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "_published_at",
+      _order: "desc",
+    },
+  });
+
+  const episodes: Episode[] = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
+
+  return {
+    props: {
+      latestEpisodes,
+      allEpisodes,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 };
 
 export default Home;
